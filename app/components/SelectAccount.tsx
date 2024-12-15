ed5e291e242a9edecadded60b7dd0a533d0dab03
Marion SALAUN <marion.salauen.001@student.uni.lu>
2024-12-15 19:59:26 +0100

    fix: avoir redundancy

--- app/components/SelectAccount.tsx
+++ app/components/SelectAccount.tsx
@@ -24,39 +24,38 @@ export const SelectAccount = () => {
     });
   }, [dispatch]);
 
-  const handleSelect1 = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectAccount1(event.target.value);
-  };
-  const handleSelect2 = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    SetSelectAccount2(event.target.value);
-  };
-
   //TODO: selection should be done with ID not name
-  const selectAccount1: Account | undefined = accounts.find(
-    (account: Account) => account.name === selectedAccountName1
-  );
-  const selectAccount2: Account | undefined = accounts.find(
-    (account: Account) => account.name === selectedAccountName2
-  );
+  const accountNamePredicate = (accountName: String) => (account: Account) =>
+    account.name === accountName;
 
   return (
     <section className="accounts-list">
       <h2>Accounts</h2>
-      <select onChange={handleSelect1} value={selectedAccountName1.toString()}>
+      <select
+        onChange={(e) => setSelectAccount1(e.target.value)}
+        value={selectedAccountName1.toString()}
+      >
         <option disabled>Select first account</option>
         {accounts.map((account: Account) => (
           <option key={Number(account.id)}>{account.name}</option>
         ))}
       </select>
-      <select onChange={handleSelect2} value={selectedAccountName2.toString()}>
+      <select
+        onChange={(e) => SetSelectAccount2(e.target.value)}
+        value={selectedAccountName2.toString()}
+      >
         <option disabled>Select second account</option>
         {accounts.map((account: Account) => (
           <option key={Number(account.id)}>{account.name}</option>
         ))}
       </select>
       <Transfer
-        accountSelected1={selectAccount1}
-        accountSelected2={selectAccount2}
+        accountSelected1={accounts.find(
+          accountNamePredicate(selectedAccountName1)
+        )}
+        accountSelected2={accounts.find(
+          accountNamePredicate(selectedAccountName2)
+        )}
       />
     </section>
   );
