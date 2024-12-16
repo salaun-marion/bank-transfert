84d059781d7e72cbcc3f462e63c068d8f7ffadcb
Marion SALAUN <marion.salauen.001@student.uni.lu>
2024-12-16 17:57:09 +0100

    feat: i18n implemented on UI

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { Link } from '@/i18n/routing';
 import img from '../../public/logo-512.png';
 import '../styles/header.css';
 
@@ -8,6 +9,12 @@ export default function Header() {
       <Image src={img} width={100} height={100} alt="logo" />
       <h1>Orange Bank</h1>
       <p>The Bank that never lies</p>
+      <Link href="/" locale="fr">
+        <button className="buttonFR buttonLanguage">FR</button>
+      </Link>
+      <Link href="/" locale="en">
+        <button className="buttonEN buttonLanguage">EN</button>
+      </Link>
     </div>
   );
 }
