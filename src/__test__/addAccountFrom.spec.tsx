77f535eedb050b357c44f6c71040ee0efa8276f8
Marion SALAUN <marion.salauen.001@student.uni.lu>
2024-12-17 12:58:19 +0100

    fix: unit test without warning

--- src/__test__/addAccountFrom.spec.tsx
+++ src/__test__/addAccountFrom.spec.tsx
@@ -5,20 +5,13 @@ import { render } from '@testing-library/react';
 import { StoreProvider } from '../app/[locale]/StoreProvider';
 import { AddAccount } from '../app/components/cards/AddAccount';
 import { balance, currency, name, title } from './constants';
+import messages from '../messages/en.json';
 
 describe('Opinion Form', () => {
-  const useRouter = jest.spyOn(require('next/router'), 'useRouter');
-  const locale = 'en';
-  const messages = require(`../messages/${locale}.json`);
-
-  useRouter.mockImplementationOnce(() => ({
-    query: { locale: locale },
-  }));
-
   it('should render', () => {
     render(
       <StoreProvider>
-        <NextIntlClientProvider messages={messages} locale="en">
+        <NextIntlClientProvider locale="en" messages={messages}>
           <AddAccount />
         </NextIntlClientProvider>
       </StoreProvider>
@@ -33,7 +26,7 @@ describe('Opinion Form', () => {
   it('should check that we cannot write more than 30 characters for name', async () => {
     render(
       <StoreProvider>
-        <NextIntlClientProvider locale="en">
+        <NextIntlClientProvider locale="en" messages={messages}>
           <AddAccount />
         </NextIntlClientProvider>
       </StoreProvider>
